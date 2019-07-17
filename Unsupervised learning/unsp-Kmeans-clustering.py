3db18000a1d817d5d6731ed40b7faa930fc85ccb
shubham860 <shubham.chauhan860@gmail.com>
2019-07-18 03:51:07 +0530

    update

--- Unsupervised learning/unsp-Kmeans-clustering.py
+++ Unsupervised learning/unsp-Kmeans-clustering.py
@@ -24,7 +24,7 @@ plt.show()
 km = KMeans(n_clusters = 5)
 y_pred = km.fit_predict(x)
 
-plt.scatter(x[y_pred == 0,0],x[y_pred == 0,1], c="r")
+plt.scatter(x[y_pred == 0,0],x[y_pred == 0,1], c="r", )
 plt.scatter(x[y_pred == 1,0],x[y_pred == 1,1], c="g")
 plt.scatter(x[y_pred == 2,0],x[y_pred == 2,1], c="y")
 plt.scatter(x[y_pred == 3,0],x[y_pred == 3,1], c="b")
